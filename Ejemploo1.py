c4c0ee2424f768c768851b9a82efb1e7a17b593b
MalosHabitos <74401232@continental.edu.pe>
2024-09-09 14:53:37 -0500

    modificacion ejercicio 01

--- Ejemploo1.py
+++ Ejemploo1.py
@@ -2,8 +2,8 @@ def multiplicacion( refactor1, factor2):
     producto = refactor1 *refactor1
     return producto
 
-if __main__=="__main__"
-    x = 10
-    y = 15
-    resultado = f(x, y)
-    print(resultado)
+if __main__=="__main__":
+    multiplicando = float(input("multiplicando: "))
+    multiplicador = float(input("multiplicandor: "))
+    resultado = multiplicacion(multiplicando, multiplicador)
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
